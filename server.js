db717cd4f1719b6afefc86a569df34b17d131897
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-08-18 08:03:18 +0200

    update 17-08-21

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const controller = require("./controller")
+const controller = require("./controller");
 
 http.createServer(controller).listen(3003, function() {
     console.log("Server startet, gå til http://localhost:3003");
